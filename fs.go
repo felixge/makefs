be46920c90fdb65ff5e5c40bcc76106dda26fe89
Felix Geisendörfer <felix@debuggable.com>
2013-01-17 13:57:50 +0100

    Move rulefs into own file

---
+++ fs.go
@@ -0,0 +1,61 @@
+package makefs
+
+/*
+Vocabulary:
+
+* pattern: a string containing exactly one % sign.
+* glob: a string containing one or more * character. Single stars expand to any
+	character except for the path separator. Double stars expand to any character,
+	including the separator.
+
+
+*/
+
+import (
+	"net/http"
+	"os/exec"
+)
+
+func NewFs(base http.FileSystem) *Fs {
+	return &Fs{head: base}
+}
+
+type Fs struct {
+	head http.FileSystem
+}
+
+func (fs *Fs) Open(path string) (http.File, error) {
+	return fs.head.Open(path)
+}
+
+func (fs *Fs) MultiMake(targets []string, sources []string, recipe Recipe) {
+	rule := &rule{
+		targets: targets,
+		sources: sources,
+		recipe:  recipe,
+	}
+
+	fs.head = &ruleFs{
+		parent: fs.head,
+		rule:   rule,
+	}
+}
+
+func (fs *Fs) Make(target string, source string, recipe Recipe) {
+	fs.MultiMake([]string{target}, []string{source}, recipe)
+}
+
+func (fs *Fs) ExecMake(target string, source string, command string, args ...string) {
+	fs.Make(target, source, func(task *Task) error {
+		cmd := exec.Command(command, args...)
+		cmd.Stdin = task.Source()
+		cmd.Stdout = task.Target()
+		cmd.Stderr = task.Target()
+
+		return cmd.Run()
+	})
+}
+
+func (fs *Fs) SubFs(newRoot string) http.FileSystem {
+	return NewSubFs(fs.head, newRoot)
+}
---
+++ fs_test.go
@@ -0,0 +1,84 @@
+package makefs
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"runtime"
+	"testing"
+)
+
+// Get name/dir of this source file
+var (
+	_, __filename, _, _ = runtime.Caller(0)
+	__dirname           = filepath.Dir(__filename)
+	fixturesDir         = __dirname + "/fixtures"
+)
+
+func TestMakeFs_Make(t *testing.T) {
+	fs := NewFs(http.Dir(fixturesDir))
+	fs.Make("%.sha1", "%.txt", func(t *Task) error {
+		hash := sha1.New()
+		if _, err := io.Copy(hash, t.Source()); err != nil {
+			return err
+		}
+		if _, err := t.Target().Write(hash.Sum(nil)); err != nil {
+			return err
+		}
+		return nil
+	})
+
+	file, err := fs.Open("/foo.sha1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	buf := new(bytes.Buffer)
+	if _, err := io.Copy(buf, file); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "781b3017fe23bf261d65a6c3ed4d1af59dea790f"
+	got := fmt.Sprintf("%x", buf)
+	if got != expected {
+		fmt.Printf("unexpected: %s\n", got)
+	}
+}
+
+func TestMakeFs_ExecMake(t *testing.T) {
+	fs := NewFs(http.Dir(fixturesDir))
+	fs.ExecMake("%.sha1", "%.txt", "cut", "-c", "1-11")
+
+	file, err := fs.Open("/foo.sha1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	buf := new(bytes.Buffer)
+	if _, err := io.Copy(buf, file); err != nil {
+		t.Fatal(err)
+	}
+
+	expected := "May the foo\n"
+	got := buf.String()
+	if got != expected {
+		fmt.Printf("unexpected: %s\n", got)
+	}
+}
+
+func TestMakeFs_SubFs(t *testing.T) {
+	fs := NewFs(http.Dir(fixturesDir))
+	subFs := fs.SubFs("/sub")
+
+	if _, err := fs.Open("/a.txt"); !os.IsNotExist(err) {
+		t.Fatal("unexpected error", err)
+	}
+
+	if _, err := subFs.Open("/a.txt"); err != nil {
+		t.Fatal("unexpected error", err)
+	}
+}
