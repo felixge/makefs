7f0b2d4768af35d550ff23db45728a50e3a96d8a
Felix Geisendörfer <felix@debuggable.com>
2013-02-07 13:57:11 +0100

    Implement IncludeFs

---
+++ includefs.go
@@ -0,0 +1,25 @@
+package makefs
+
+import (
+	"net/http"
+	"os"
+	"strings"
+)
+
+func NewIncludeFs(base http.FileSystem, includes []string) http.FileSystem {
+	return &IncludeFs{base: base, includes: includes}
+}
+
+type IncludeFs struct {
+	base http.FileSystem
+	includes []string
+}
+
+func (fs *IncludeFs) Open(path string) (http.File, error) {
+	for _, include := range fs.includes {
+		if strings.HasPrefix(path, include) {
+			return fs.base.Open(path)
+		}
+	}
+	return nil, &os.PathError{"open", path, os.ErrNotExist}
+}
---
+++ includefs_test.go
@@ -0,0 +1,30 @@
+package makefs
+
+import (
+	"net/http"
+	"os"
+	"testing"
+)
+
+func TestIncludeFs_Open(t *testing.T) {
+	fs := NewIncludeFs(http.Dir(fixturesDir), []string{"/sub/", "/sub3/"})
+
+	// included, should exist
+	if file, err := fs.Open("/sub/a.txt"); err != nil {
+		t.Fatal(err)
+	} else {
+		file.Close()
+	}
+
+	// included, should exist
+	if file, err := fs.Open("/sub3/sub3.txt"); err != nil {
+		t.Fatal(err)
+	} else {
+		file.Close()
+	}
+
+	// not included, should not exist
+	if _, err := fs.Open("/sub2/sub2.txt"); !os.IsNotExist(err) {
+		t.Fatal(err)
+	}
+}
